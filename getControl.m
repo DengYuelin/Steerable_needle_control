3e99c5258edd755c73158ef1858345a4358fb86b
DengYuelin <dengyuelin66dyl@163.com>
2022-02-16 00:00:10 -0500

    add Rotation limitations

--- getControl.m
+++ getControl.m
@@ -1,27 +1,39 @@
-function u = getControl(lambda1, lambda2, sigma, epsilon, traj)
+function u = getControl(lambda1, lambda2, sigma, epsilon, Rlimit, traj)
     u = zeros(2, 1);
 
-    if (nargin < 4)
+    if (nargin == 3)
         u(1) = lambda1;
         u(2) = lambda2 * sign(sigma);
         return
-    elseif (nargin < 5)
+    elseif (nargin == 5)
         u(1) = lambda1;
 
-        if abs(sigma) > epsilon
-            u(2) = lambda2 * sign(sigma);
+        if Rlimit.W == 1
+            u(2) = -lambda2 * sign(Rlimit.theta);
         else
-            u(2) = lambda2 * sigma / epsilon;
+
+            if abs(sigma) > epsilon
+                u(2) = lambda2 * sign(sigma);
+            else
+                u(2) = lambda2 * sigma / epsilon;
+            end
+
         end
 
         return
-    elseif (nargin < 6)
+    elseif (nargin == 6)
         u(1) = traj.pdot + traj.kp * traj.ez;
 
-        if abs(sigma) > epsilon
-            u(2) = lambda2 / lambda1 * sign(sigma) * norm(u(1));
+        if Rlimit.W == 1
+            u(2) = -lambda2 / lambda1 * sign(Rlimit.theta) * norm(u(1));
         else
-            u(2) = lambda2 / lambda1 * sigma / epsilon * norm(u(1));
+
+            if abs(sigma) > epsilon
+                u(2) = lambda2 / lambda1 * sign(sigma) * norm(u(1));
+            else
+                u(2) = (lambda2 / lambda1) * (sigma / epsilon) * norm(u(1));
+            end
+
         end
 
         return
--- testTrajFollowing.m
+++ testTrajFollowing.m
@@ -1,14 +1,14 @@
 clc, clear, close all
 %%
 T = 25; % total time 25s
-dt = 0.01;
+dt = 0.001;
 tspan = 0:dt:T;
 
 % Constants
 lambda1 = 0.005; % m/s, equal to 5 mm/s
 k = 10;
 lambda2 = 2 * pi; % rad/s
-epsilon = 1;
+epsilon = deg2rad(1);
 % initial state
 p0 = [0; 0; 0];
 eul0 = [0, 0, 0];
@@ -33,8 +33,10 @@ Rt = R0;
 
 Hist.position = [];
 Hist.error = []; % error vector
+Rlimit.W = 0;
+Rlimit.theta = 0;
 
-for i = 1:size(tspan, 2)-1
+for i = 1:size(tspan, 2) - 1
     t = tspan(i);
     et = getErrorVec(Rt, pt, Pd(:, i + 1));
     [sigma, sigma_sign] = getManifold(et);
@@ -45,12 +47,19 @@ for i = 1:size(tspan, 2)-1
     traj.ez = et(3);
     traj.kp = 0.5;
 
-    ut = getControl(lambda1, lambda2, sigma, epsilon, traj);
+    ut = getControl(lambda1, lambda2, sigma, epsilon, Rlimit, traj);
     % Simulate one step dynamics
     [Rnext, pnext] = dynamics_dt(Rt, pt, ut, k, dt);
 
     Rt = Rnext;
     pt = pnext;
+    Rlimit.theta = Rlimit.theta + ut(2) * dt;
+
+    if abs(Rlimit.theta) > deg2rad(270)
+        Rlimit.W = 1;
+    elseif abs(Rlimit.theta) < deg2rad(45)
+        Rlimit.W = 0;
+    end
 
     Hist.position = [Hist.position pnext];
     Hist.error = [Hist.error norm(et)];
@@ -81,5 +90,5 @@ set(gcf, 'position', [100, 100, 700, 700])
 % zlabel('z');
 %%
 figure(4)
-plot(tspan(1:end-1), Hist.error)
+plot(tspan(1:end - 1), Hist.error)
 set(gca, 'YScale', 'log')
